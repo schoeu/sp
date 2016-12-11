48d8695c4f2681f263598d8ab0c3ade917f1f1b9
schoeu <quanyou1013@gmail.com>
2016-12-11 18:17:58 +0800

    update config module

--- server/main.go
+++ server/main.go
@@ -2,6 +2,44 @@
 
 package main
 
-func main() {
+import (
+    "encoding/json"
+    "io/ioutil"
+    "log"
+	"fmt"
+)
 
+
+type ConfProxy struct{
+    Server string `json:"server"`
+    LocalAddress string `json:"local_address"`
+    LocalPort string `json:"local_port"`
+    PortPassword PortPassword `json:"port_password"`
+    Timeout int `json:"timeout"`
+    Method string `json:"method"`
+    FastOpen bool `json:"fast_open"`
+}
+
+type PortPassword map[string]string
+
+
+func main() {
+    conf, err := conf.LoadConf("../../conf.json")
+    if err != err {
+        log.Fatal("conf error")
+    }
+    fmt.Println(conf)
+}
+
+func LoadConf(p string) (*ConfProxy, error){
+    content, err := ioutil.ReadFile(p)
+    if err != nil {
+        return nil, err
+    }
+    cp := &ConfProxy{}
+    jsonErr := json.Unmarshal(content, cp)
+    if jsonErr != nil {
+        return nil, err 
+    }
+    return cp, nil
 }
